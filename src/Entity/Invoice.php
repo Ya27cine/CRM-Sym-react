cb141c330dcb76deadc5a88ce5cf5beff765b952
KHELIFA Yassine <prostam27@gmail.com>
2022-04-23 17:27:55 +0200

    Install ApiPlatforme

--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -4,9 +4,11 @@ namespace App\Entity;
 
 use App\Repository\InvoiceRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Entity(repositoryClass=InvoiceRepository::class)
+ * @ApiResource
  */
 class Invoice
 {
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,10 +7,13 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use ApiPlatform\Core\Annotation\ApiResource;
+
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\Table(name="`user`")
+ * @ApiResource
  */
 class User implements UserInterface
 {

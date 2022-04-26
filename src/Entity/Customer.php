d169f0af2b3efdadb5601b182c60bb1f6ffc2449
KHELIFA Yassine <prostam27@gmail.com>
2022-04-26 18:57:25 +0200

    Activate / deactivate operations on a resource

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -15,6 +15,8 @@ use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 /**
  * @ORM\Entity(repositoryClass=CustomerRepository::class)
  * @ApiResource(
+ *          collectionOperations={"GET", "POST"},
+ *          itemOperations={"GET", "PUT", "DELETE"},
  *          normalizationContext={
  *                          "groups"={"customers_read"}
  *          }

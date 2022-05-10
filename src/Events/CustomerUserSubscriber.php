66ce3f99c606bcf5c869ea9386f3d4f6963d660b
KHELIFA Yassine <prostam27@gmail.com>
2022-05-10 22:12:16 +0200

    set user for customer

---
+++ src/Events/CustomerUserSubscriber.php
@@ -0,0 +1,39 @@
+<?php 
+
+namespace App\Events;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Security;
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\Customer;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+
+class CustomerUserSubscriber implements EventSubscriberInterface{
+
+    private $user;
+
+    public function __construct(Security $security)
+    {
+        $this->user = $security->getUser();
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ["putUserInCustomer", EventPriorities::PRE_VALIDATE]
+        ];
+    }
+
+    public function setUserForCustomer(ViewEvent $event){
+        $customer = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if( $method === "POST" && $customer instanceof Customer)
+        {
+           $customer->setUserUp( $this->user );
+        }
+    }
+
+
+}

d4e8c42ab1618c850e670b143b103a4e68a707fc
KHELIFA Yassine <prostam27@gmail.com>
2022-05-10 23:45:01 +0200

    set chrono & sent_at  for Invoice

--- src/Events/InvoiceChronoSubscriber.php
+++ src/Events/InvoiceChronoSubscriber.php
@@ -4,17 +4,30 @@ namespace App\Events;
 
 use ApiPlatform\Core\EventListener\EventPriorities;
 use App\Entity\Invoice;
+use App\Repository\InvoiceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Security;
 
 class InvoiceChronoSubscriber implements EventSubscriberInterface{
 
+    private $em;
+    private $invoiceRepo;
+    private $user;
+
+    public function __construct(EntityManagerInterface $em,Security $security ,InvoiceRepository $invoiceRepository)
+    {
+        $this->em = $em;
+        $this->user = $security->getUser();
+        $this->invoiceRepo = $invoiceRepository;
+    }
 
     public static function getSubscribedEvents()
     {
         return [
-            KernelEvents::VIEW => ['incrementChrono', EventPriorities::PRE_WRITE]
+            KernelEvents::VIEW => ['incrementChrono', EventPriorities::PRE_VALIDATE]
         ];
     }
 
@@ -24,8 +37,13 @@ class InvoiceChronoSubscriber implements EventSubscriberInterface{
         $method = $event->getRequest()->getMethod();
 
         if( $invoice instanceof Invoice && $method === "POST" ){
-            $value = $invoice->getChrono() + 1;
-            dd($value);
+            //$value = $invoice->getChrono() + 1;
+            $value = $this->invoiceRepo->findLastChrono($this->user);
+          //  dd($value);
+            $invoice->setChrono($value);
+
+            if($invoice->getSentAt() == null)
+                $invoice->setSentAt(new \DateTime());
         } 
     }
 }

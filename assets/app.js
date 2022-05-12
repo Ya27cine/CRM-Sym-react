3afbee36d27ef0c10be8c4cb34e84e511873d281
KHELIFA Yassine <prostam27@gmail.com>
2022-05-13 00:34:16 +0200

    Setting up routing with React Router Dom

--- assets/app.js
+++ assets/app.js
@@ -3,17 +3,22 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import { HashRouter, Switch , Route} from 'react-router-dom';
 
 import './styles/app.css';
+import Customer from './pages/Customer';
 
 const App = () =>{
     return (
-    <div>
+    <HashRouter>
         <Navbar />
-        <div class="container pt-5">
-          <Home/>
-        </div>
-    </div>
+        <main class="container pt-5">
+           <Switch>
+             <Route path="/customers" component={Customer} />
+             <Route path="/" component={Home} />
+           </Switch>
+        </main>
+    </HashRouter>
     );
 }
 
---
+++ assets/pages/Customer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+const Customer = () => {
+    return ( <h1>Customer list</h1> );
+}
+ 
+export default Customer;
